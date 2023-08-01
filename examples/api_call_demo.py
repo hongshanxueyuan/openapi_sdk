4c4030363540ca9a6dfef1c0632f221a9e5507b1
duoduo369 <duoduo3369@gmail.com>
2023-08-01 10:56:56 +0800

    update api_call_demo

--- examples/api_call_demo.py
+++ examples/api_call_demo.py
@@ -2,17 +2,23 @@ from firacademy_openapi.client import Client
 
 APP_KEY = 'your-app-key'
 APP_SECRET = 'your-app-secret'
+HOST = 'https://openapi.firacademy.com'
+
+
+def get_client():
+    client = Client(APP_KEY, APP_SECRET, timeout=30)
+    return client
 
 
 def get_no_param_request():
-    url= 'http://bbafae24be8d4cfeb7524831fd3f2c63-cn-zhangjiakou.alicloudapi.com/openapi/v2/courses/'
-    client = Client(APP_KEY, APP_SECRET, timeout=30)
+    url= '{}/openapi/v2/courses/'.format(HOST)
+    client = get_client()
     resp = client.request(url, 'GET')
     return resp
 
 
 def get_authenticate_and_param_request():
-    url= 'http://bbafae24be8d4cfeb7524831fd3f2c63-cn-zhangjiakou.alicloudapi.com/openapi/v1/enrollments/'
+    url= '{}/openapi/v1/enrollments/'.format(HOST)
     headers = {
         'Authorization': 'Bearer f8rGP14RLbiWAjCki5fVEUCroqF2as',
     }
@@ -20,14 +26,14 @@ def get_authenticate_and_param_request():
         'offset': '0',
         'limit': '1',
     }
-    client = Client(APP_KEY, APP_SECRET, timeout=30)
+    client = get_client()
     resp = client.request(url, 'GET', params=params, headers=headers)
     return resp
 
 
 def post_data_request():
-    url= 'http://bbafae24be8d4cfeb7524831fd3f2c63-cn-zhangjiakou.alicloudapi.com/openapi/v1/auth/login/'
-    client = Client(APP_KEY, APP_SECRET, timeout=30)
+    url= '{}/openapi/v1/auth/login/'.format(HOST)
+    client = get_client()
     data = {
         'password': 'your-password',
         'account': 'your-email@email.com',
